4f5e65d97c16da82cb0cf1b627c2f24001f46f0c
Andrew Koster <andrewkoster95@gmail.com>
2021-07-09 14:31:43 -0400

    Added function to reutrn license badge to generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,44 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseBadge;
+
+  switch (license) {
+    //is a match, then return the declared license badge
+    case "APACHE 2.0":
+      licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+      return licenseBadge
+      
+    case "BSD 3":
+      licenseBadge = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+      return licenseBadge
+
+    case "ECLIPSE 1.0":
+      licenseBadge = "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
+      return licenseBadge
+
+    case "GPL V3":
+      licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+      return licenseBadge
+
+    case "IBM 1.0":
+      licenseBadge = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+      return licenseBadge
+
+    case "ISC":
+      licenseBadge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
+      return licenseBadge
+
+    case "MIT":
+      licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+      return licenseBadge
+
+    //default 
+    default:
+      licenseBadge = ""
+      return licenseBadge  
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
